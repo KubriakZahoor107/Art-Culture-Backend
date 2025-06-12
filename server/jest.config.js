a17ff94776f07b425f8d019a3b19b19747027acd
Костянтин Кубряк <konstantin.kubriak@gmail.com>
2025-06-12 13:47:33 +0300

    docs: clarify test instructions

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,6 +1,10 @@
 export default {
+  preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.js', '.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   transform: {
     '^.+\\.(t|j)sx?$': ['ts-jest', { useESM: true }],
   },
